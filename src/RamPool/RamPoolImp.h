d20b4c5b72e52c9bb3a7ab46c00fad669bde55fa
byfxxm <byfxxm@outlook.com>
2021-09-14 19:51:55 +0800

    good

--- src/RamPool/RamPoolImp.h
+++ src/RamPool/RamPoolImp.h
@@ -7,13 +7,13 @@ class CRamPoolImp
 public:
 	CRamPoolImp();
 	~CRamPoolImp();
+	static CRamPoolImp* Instance();
 	void* Malloc(size_t);
 	void Free(void*);
 	void* Realloc(void*, size_t);
 	void Destroy();
 	void Leak(LeakInfo*);
 	size_t Size(void*);
-	static CRamPoolImp* Instance();
 
 private:
 	CPool m_Pools[POOLNUM];
