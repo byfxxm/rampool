7ef01e2418be4542ada3d768a79048d7027f0b9a
byfxxm <byfxxm@outlook.com>
2021-09-19 17:48:58 +0800

    22

--- src/RamPool/dllmain.cpp
+++ src/RamPool/dllmain.cpp
@@ -1,10 +1,6 @@
-﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
-#include "stdafx.h"
+﻿#include "stdafx.h"
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-					   DWORD  ul_reason_for_call,
-					   LPVOID lpReserved
-					 )
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
 	switch (ul_reason_for_call)
 	{
