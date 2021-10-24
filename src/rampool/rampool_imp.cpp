97f5793bcfdd7729ca76f3883ce2f82c4e5c8bc8
byfxxm <byfxxm@outlook.com>
2021-10-24 14:35:44 +0800

    22

--- src/rampool/rampool_imp.cpp
+++ src/rampool/rampool_imp.cpp
@@ -52,7 +52,6 @@ void* rampool_imp::realloc(void* p, size_t size)
 		return malloc(size);
 
 	auto slt = __slot_cast(p);
-
 	if (size <= slt->normalize_size)
 	{
 		__pools[POOLINDEX(slt->normalize_size)].total() += size - slt->actual_size;
