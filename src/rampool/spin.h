a90ac385991347317c54a7399277bba0cf9ca1dc
byfxxm <byfxxm@outlook.com>
2022-03-26 17:47:07 +0800

    22

--- src/rampool/spin.h
+++ src/rampool/spin.h
@@ -7,16 +7,16 @@ namespace rampool
 	public:
 		void lock()
 		{
-			while (flag.test_and_set(std::memory_order_relaxed))
+			while (__flag.test_and_set(std::memory_order_relaxed))
 				std::this_thread::yield();
 		}
 
 		void unlock()
 		{
-			flag.clear(std::memory_order_relaxed);
+			__flag.clear(std::memory_order_relaxed);
 		}
 
 	private:
-		std::atomic_flag flag;
+		std::atomic_flag __flag;
 	};
 }
